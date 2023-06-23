868a765f1b92ee7e891d6d8d0e63284a1262eb1c
Orlando Cedeno <ocedenos@uteq.edu.ec>
2023-06-22 23:49:05 -0500

    Deber terminado: Diseño, Funcionalidad y Validaciones

--- app/src/main/java/com/example/debern1_cedenoorland/DatosRegistrados.java
+++ app/src/main/java/com/example/debern1_cedenoorland/DatosRegistrados.java
@@ -2,36 +2,39 @@ package com.example.debern1_cedenoorland;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ResourceBundle;
 
 public class DatosRegistrados extends AppCompatActivity {
 
+    TextView lbltitulo,lblcedula,lblnombres,lblfechanaci,lblciudad,lblgenero,lblcorreo,lbltelefono;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_registrados);
 
-        TextView lbltitulo = (TextView) findViewById(R.id.lblTitulo2);
-        TextView lblcedula = (TextView) findViewById(R.id.lblCedula3);
-        TextView lblnombres = (TextView) findViewById(R.id.lblNombres);
-        TextView lblfechanaci = (TextView) findViewById(R.id.lblFechaNaci);
-        TextView lblciudad = (TextView) findViewById(R.id.lblCiudad2);
-        TextView lblgenero = (TextView) findViewById(R.id.lblGenero2);
-        TextView lblcorreo = (TextView) findViewById(R.id.lblCorreo2);
-        TextView lbltelefono = (TextView) findViewById(R.id.lblTelefono);
+         lbltitulo = (TextView) findViewById(R.id.lblTitulo2);
+         lblcedula = (TextView) findViewById(R.id.lblCedula3);
+         lblnombres = (TextView) findViewById(R.id.lblNombres);
+         lblfechanaci = (TextView) findViewById(R.id.lblFechaNaci);
+         lblciudad = (TextView) findViewById(R.id.lblCiudad2);
+         lblgenero = (TextView) findViewById(R.id.lblGenero2);
+         lblcorreo = (TextView) findViewById(R.id.lblCorreo2);
+         lbltelefono = (TextView) findViewById(R.id.lblTelefono);
+
 
         Bundle bundle = this.getIntent().getExtras();
 
-        lbltitulo.setText(" Hola! sus datos ingresados son:");
+        lbltitulo.setText(" Hola! Sus datos ingresados son: ");
         lblcedula.setText(" Cédula: " + bundle.getString("CEDULA"));
         lblnombres.setText(" Nombres: " + bundle.getString("NOMBRES"));
         lblfechanaci.setText(" Fecha de Nacimiento: " + bundle.getString("FechaNac"));
         lblciudad.setText(" Ciudad: " + bundle.getString("CIUDAD"));
         lblgenero.setText(" Género: " + bundle.getString("MASCULINO"));
-        //lblcorreo.setText(" Género \n " + bundle.getString("FEMENINO"));
         lblcorreo.setText(" Correo: " + bundle.getString("CORREO"));
         lbltelefono.setText(" Teléfono: " + bundle.getString("TELEFONO"));
     }
--- app/src/main/java/com/example/debern1_cedenoorland/MainActivity.java
+++ app/src/main/java/com/example/debern1_cedenoorland/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,21 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void CargaComponentes()
-    {
 
     }
+    EditText txtcedula,txtnombres,txtfecha,txtciudad,txtcorreo,txttelefono;
+    RadioButton rbmasculino,rbfemenino;
+
+    int comprobador=0;
+
     public void btnEnviar(View view){
-        EditText txtcedula = (EditText)findViewById(R.id.txtCedula);
-        EditText txtnombres = (EditText)findViewById(R.id.txtNombre);
-        EditText txtfecha = (EditText)findViewById(R.id.txtFechaNac);
-        EditText txtciudad = (EditText)findViewById(R.id.txtCiudad);
-        RadioButton rbmasculino = (RadioButton) findViewById(R.id.rbMasculino);
-        RadioButton rbfemenino = (RadioButton)findViewById(R.id.rbFemenino);
-        EditText txtcorreo = (EditText)findViewById(R.id.txtCorreo);
-        EditText txttelefono = (EditText)findViewById(R.id.txtTelefono);
+
+        txtcedula = (EditText)findViewById(R.id.txtCedula);
+        txtnombres  = (EditText)findViewById(R.id.txtNombre);
+        txtfecha = (EditText)findViewById(R.id.txtFechaNac);
+        txtciudad = (EditText)findViewById(R.id.txtCiudad);
+        rbmasculino = (RadioButton) findViewById(R.id.rbMasculino);
+        rbfemenino = (RadioButton)findViewById(R.id.rbFemenino);
+        txtcorreo = (EditText)findViewById(R.id.txtCorreo);
+        txttelefono = (EditText)findViewById(R.id.txtTelefono);
+
+        //Comprobar la selección del genero para el usuario
+        String selecrb = rbmasculino.isChecked()?"MASCULINO":"FEMENINO";
+
         Intent intent = new Intent(MainActivity.this, DatosRegistrados.class);
 
         Bundle bndl = new Bundle();
@@ -36,30 +44,54 @@ public class MainActivity extends AppCompatActivity {
         bndl.putString("NOMBRES", txtnombres.getText().toString());
         bndl.putString("FechaNac", txtfecha.getText().toString());
         bndl.putString("CIUDAD", txtciudad.getText().toString());
-        bndl.putString("MASCULINO", rbmasculino.getText().toString());
-        //bndl.putString("FEMENINO", rbmasculino.getText().toString());
+        bndl.putString("MASCULINO", selecrb);
         bndl.putString("CORREO", txtcorreo.getText().toString());
         bndl.putString("TELEFONO", txttelefono.getText().toString());
 
+
+        ValidarVacios();
+
         intent.putExtras(bndl);
-
+        if (comprobador==1)
         startActivity(intent);
+        else
+            comprobador=0;
     }
+    //Método de limpieza sencillo
     public void Limpieza(View v) {
-        EditText txtcedula = (EditText)findViewById(R.id.txtCedula);
-        EditText txtnombres = (EditText)findViewById(R.id.txtNombre);
-        EditText txtfecha = (EditText)findViewById(R.id.txtFechaNac);
-        EditText txtciudad = (EditText)findViewById(R.id.txtCiudad);
-        RadioButton rbmasculino = (RadioButton) findViewById(R.id.rbMasculino);
-        RadioButton rbfemenino = (RadioButton)findViewById(R.id.rbFemenino);
-        EditText txtcorreo = (EditText)findViewById(R.id.txtCorreo);
-        EditText txttelefono = (EditText)findViewById(R.id.txtTelefono);
-
         txtcedula.setText("");
         txtnombres.setText("");
         txtfecha.setText("");
         txtciudad.setText("");
         txtcorreo.setText("");
         txttelefono.setText("");
+        rbmasculino.setChecked(false);
+        rbfemenino.setChecked(false);
+    }
+
+    //Declaración de variables para cada texts
+    public void ValidarVacios()
+    {
+        String cd = txtcedula.getText().toString(),
+                nb=txtnombres.getText().toString(),
+                fh=txtfecha.getText().toString(),
+                cid=txtciudad.getText().toString(),
+                crre=txtcorreo.getText().toString(),
+                telf=txttelefono.getText().toString();
+        //Condiciónal para verificar el estado vacío de cada text
+        if (cd.isEmpty() || nb.isEmpty() || fh.isEmpty() || cid.isEmpty() || crre.isEmpty() || telf.isEmpty())
+        {
+            txtcedula.setError("El campo está vacío");
+            txtnombres.setError("El campo está vacío");
+            txtfecha.setError("El campo está vacío");
+            txtciudad.setError("El campo está vacío");
+            txtcorreo.setError("El campo está vacío");
+            txttelefono.setError("El campo está vacío");
+            comprobador=0;
+        }
+        else
+        {
+            comprobador=1 ;
+        }
     }
 }
